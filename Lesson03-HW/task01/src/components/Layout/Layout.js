aa5da31633afd32dd939aa78959e03e329eb7092
Abzal <Abzal.Tankibayev@outlook.com>
2019-07-24 14:02:02 +0600

    Intermediate commit for Lesson03-HW - added Layout, Menu, Login v1.0.0

---
+++ Lesson03-HW/task01/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Layout.css';
+import Menu from '../Menu/index';
+
+function Layout(){
+    return (
+        <div>
+            <Menu/>           
+        </div>
+        );
+}
+export default Layout;
---
+++ Lesson03-HW/task01/src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Login.css';
+
+function Login(){
+    return (
+        <div>
+            
+       
+        </div>
+              
+    );
+}
+export default Login;
+
+ 
